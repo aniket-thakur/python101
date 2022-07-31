72a17a2b2efb69685066bbca6a845b650df1b1c4
Aniket Thakur <anikett126@gmail.com>
2022-08-01 01:38:07 +0530

    Added requirement.txt file and some updates

---
+++ main.py
@@ -0,0 +1,50 @@
+import numpy as np
+# print("hello", "there", sep= ",")
+# print("Hello world","Iam Aniket","Iam 20 year old" , sep=". ")
+# print("hello", end =" ")
+# print ("world")
+#
+# print("Hello", "India", end=", ")
+# print("Hello", "World", sep=", ", end=".")
+
+# output - > Hello India, Hello, Wrold.
+
+# ---------------------- Input from user===========================
+
+# print("Enter a number", end= " ")
+# x = int(input())
+# print("Entered number is: ", x)
+
+# # -------------------------type casting===================================
+# x =20
+# y = 29
+# print(str(x)+ str(y))
+# s = 'a'
+# print(ord(s))                 # gives the ascii value of a character
+
+# -------------- printing start like pattern--------------------------------------------
+# eg -- *
+#       * *
+#       * * *
+#       * * * *
+#       * * * * *
+
+# for i in range(1,5):
+#     for j in range(0,i):
+#         print("*" , end=" ")
+#     print()                               // vitualenv ===  vnv
+
+# ----------------------------- Pattern question-------------------------------------------
+
+# for i in range(0,4):
+#     for j in range(0,7):
+#         if(3-i <= j <= 3+i):
+#             print("*", end = " ")
+#         else:
+#             print(" ", end = " ")
+#     print()
+
+#  Virtualenv-----------------------------------------------
+import numpy as np
+a =np.zeros((3,2))
+print(a)
